e6e0b45d4817fe249672e5bb58af46ca9e9b3d06
Manish Rao <144836697+rao-manish-24@users.noreply.github.com>
2025-03-18 20:34:52 -0400

    Update Driver.java

--- src/main/java/edu/neu/csye7374/Driver.java
+++ src/main/java/edu/neu/csye7374/Driver.java
@@ -11,7 +11,7 @@ public class Driver {
 		System.out.println("============Main Execution Start===================\n\n");
 
          //Add your code in between these two print statements
-		 
+		Restaurant.demo();
 		System.out.println("\n\n============Main Execution End===================");
 	}
 
